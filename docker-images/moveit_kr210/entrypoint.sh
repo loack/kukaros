1b5b78513b3090bbff65eeb08dbb21d26541b993
Loic Tournier <loic.tournier@gmail.com>
2025-06-07 01:13:38 +0200

    moveit config assistant working. creating another docker configuration to run moveit with one robot

---
+++ docker-images/moveit_kr210/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+cd /root/ws_moveit
+colcon build --packages-select kr210_urdf
+source /root/ws_moveit/install/setup.bash
+#ros2 launch moveit_setup_assistant setup_assistant.launch.py
+
+exec /bin/bash
+
+#chmod +x /home/lolo/kukabots/kukaros/docker/moveit_tuto/entrypoint.sh
+
+#cd /root/ws_moveit/install/kr210_urdf/share/kr210_urdf/meshes/collision/link_5.stl
---
+++ docker-images/moveit_tuto/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+DOCKER_IMAGE=main-jazzy-tutorial-source docker-compose run gpu
+
+#DOCKER_IMAGE=humble-humble-tutorial-source docker compose run cpu
+#ros2 launch moveit2_tutorials demo.launch.py
