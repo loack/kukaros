3a653d7787d2a78c6a9c27477aa425fb31132dc7
Loic Tournier <loic.tournier@gmail.com>
2025-06-07 13:43:53 +0200

    interfacing moveit with python

--- docker-images/moveit_kr210/entrypoint.sh
+++ docker-images/moveit_kr210/entrypoint.sh
@@ -2,9 +2,11 @@
 set -e
 
 cd /root/ws_moveit
-colcon build --packages-select kr210_urdf
+colcon build --packages-select kr210_urdf moveit_config
 source /root/ws_moveit/install/setup.bash
-#ros2 launch moveit_setup_assistant setup_assistant.launch.py
+#ros2 launch moveit_config demo.launch.py
+#ros2 launch moveit_config move_group.launch.py
+ros2 launch moveit2_tutorials motion_planning_python_api_tutorial.launch.py
 
 exec /bin/bash
 
--- docker-images/readme.md
+++ docker-images/readme.md
@@ -20,3 +20,12 @@ ros2 launch moveit2_tutorials demo.launch.py
 ros2 launch moveit_setup_assistant setup_assistant.launch.py
 
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/root/kuka_experimental/kuka_kr210_support
+
+
+run with : 
+
+cd /kukabots/kukaros/docker-images/moveit_kr210
+DOCKER_IMAGE=main-jazzy-tutorial-source docker-compose run gpu
+
+new terminal : 
+docker run -it moveit/moveit2:main-jazzy-tutorial-source
